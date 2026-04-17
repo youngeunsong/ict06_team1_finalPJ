718665a8c5cfa7069595a8a95845ade36f29e092
youngeunsong <yesongee93@gmail.com>
2026-04-17 16:04:04 +0900

    프로젝트 아키텍쳐대로 폴더 생성 완료

---
+++ react-frontend/src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@coreui/coreui/dist/css/coreui.min.css'
+import React from 'react'
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+        test page
+        <Sidebar />
+      
+    </div>
+  );
+}
+
+export default App;
