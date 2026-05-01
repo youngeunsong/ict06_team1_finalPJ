dbe1b324930b377beceeecda12fd1f599d53bc1d
youngeunsong <yesongee93@gmail.com>
2026-05-01 18:47:39 +0900

    테이블 삭제 쿼리 추가

--- DB/AdminApproval_v260501.sql
+++ DB/AdminApproval_v260501.sql
@@ -8,9 +8,10 @@
 delete from app_form 
  where form_id <= 14;  
 
--- (주의) 아래에 작성된 쿼리들은 실행 전에 충분히 고민할 것. 연관 테이블 삭제 위험
+-- (주의) 아래에 작성된 쿼리들은 실행 전에 충분히 고민할 것. 연관 테이블 삭제 위험-----------
 -- 비상 시에 테이블 아예 삭제
 drop table app_form cascade;  
+drop table attendance; 
 
 -- 테스트 용으로 만든 근로자 데이터 삭제 
 delete from employee 
